acbb2a0223a5b39200de48901ac4c624a0285dec
Diego de Massy <ddemassy@gmail.com>
2022-08-22 23:45:06 +0200

    ajout de la recette de tiramisu

---
+++ sites/default/files/php/twig/6303ee9583f2d___string_template__b3e375_GojQoPo_IGBJWRnWFkt1iNe4P/-jDMmTQAwev43EiFNNY90IJ0pgipA58EUTd4q20QgBQ.php
@@ -0,0 +1,91 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__b3e3752fac442f4dd6285302b3970320a0c76d95d4aaaee5f2ff93395dc8069b */
+class __TwigTemplate_2c6b92d3825f4f59a6dd04b2514b38f4872b00a5afd704b8bd1742b00d90ef5e extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<p class=\"media-library-add-form__description\">";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["text"] ?? null), 1, $this->source), "html", null, true);
+        echo "</p>";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__b3e3752fac442f4dd6285302b3970320a0c76d95d4aaaee5f2ff93395dc8069b";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{# inline_template_start #}<p class=\"media-library-add-form__description\">{{ text }}</p>", "__string_template__b3e3752fac442f4dd6285302b3970320a0c76d95d4aaaee5f2ff93395dc8069b", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 1);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
